f42fda559d71ed8a9bc9467a0fd7358b5d6ab6de
Jeet <98499268+JeetRH@users.noreply.github.com>
2022-05-12 18:31:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_nerds():
-    return "<p>Hello Nerds here I am again!! Auto refresh please please please!</p>"
+    return "<p>Hello Nerds! This is a Serverless App!</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
